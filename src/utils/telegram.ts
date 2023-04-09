06188a90d759abb028569d4eed9b19a9281bc1a9
david-fernando <davidfernandodamata21@gmail.com>
2023-04-09 18:19:18 -0300

    Update telegram.ts

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -7,10 +7,10 @@ const bot = new Telegraf(`${process.env.BOT_TOKEN}`);
 
 const isProductionEnvironment = process.env.NODE_ENV === 'production'
 
-const webHookUrl = `${process.env.VERCEL_URL}/api/bot/bot${process.env.BOT_TOKEN}`
+// const webHookUrl = `${process.env.VERCEL_URL}/api/bot/bot${process.env.BOT_TOKEN}`
 
-if(isProductionEnvironment){
-  bot.telegram.setWebhook(webHookUrl)
-}
+// if(isProductionEnvironment){
+//   bot.telegram.setWebhook(webHookUrl)
+// }
 
 export default bot
