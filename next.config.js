a5c204b133635edd65bdca3e452444693b3a8c13
david-fernando <davidfernandodamata21@gmail.com>
2023-04-09 23:27:51 -0300

    Remove serverRuntimeConfig

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
-const { spawn } = require('child_process');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  async serverRuntimeConfig() {
-    const setupProcess = spawn('npm', ['run', 'setup'], {
-      stdio: 'inherit',
-      shell: true,
-    });
-    await new Promise((resolve) => setupProcess.on('exit', resolve));
-    return {};
-  } ,
-  nextConfig
-}
+module.exports = nextConfig
