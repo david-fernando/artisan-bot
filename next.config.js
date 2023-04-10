63f1e1312a8d2cf5d9e2b2a46e6f7c3eeff90165
david-fernando <davidfernandodamata21@gmail.com>
2023-04-09 23:36:21 -0300

    Add serverRuntimeConfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    entry: './server.ts',
+  },
 }
 
-module.exports = nextConfig
+module.exports =  nextConfig
