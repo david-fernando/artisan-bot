b672162a0ea9bc3dc3250963926e4635f9bd360e
david-fernando <davidfernandodamata21@gmail.com>
2023-04-09 23:08:07 -0300

    Update telegram.ts

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -3,7 +3,18 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
+const webHookUrl = `${process.env.VERCEL_URL}/api/bot`
+
 const bot = new Telegraf(`${process.env.BOT_TOKEN}`);
+
+(async()=>{
+  await bot.launch({
+    webhook: {
+      domain: webHookUrl,
+      secretToken: process.env.BOT_TOKEN
+    }
+  })
+})()
 
 
 export default bot
