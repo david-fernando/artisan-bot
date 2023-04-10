bd2d47a03fda3385da1ac7787be23d015aafa428
david-fernando <davidfernandodamata21@gmail.com>
2023-04-09 23:06:59 -0300

    Update bot.ts

--- src/pages/api/bot.ts
+++ src/pages/api/bot.ts
@@ -8,14 +8,12 @@ const sorryMessage = `Sinto muito, mas ainda não posso executar nenhum comando!
 Estou em desenvolvimento, mas quando estiver pronta serei capaz de variais coisas legais como editar suas fotos, ou transforma-las em artes.`
 
 
-async function apiBot(_:any, response: NextApiResponse){    
+function apiBot(_:any, response: NextApiResponse){    
   bot.start((context) => context.reply(initialMessage));
     
   bot.on('text', (context) => context.reply(sorryMessage));
 
   response.json({ message: 'All nice!' })
-
-  await bot.launch()
 
 }
 
