11a631da15e9fd43bab3c7c68559211da1f79bc8
david-fernando <davidfernandodamata21@gmail.com>
2023-04-09 23:22:46 -0300

    Add serverRuntimeConfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+const { spawn } = require('child_process');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  async serverRuntimeConfig() {
+    const setupProcess = spawn('npm', ['run', 'setup'], {
+      stdio: 'inherit',
+      shell: true,
+    });
+    await new Promise((resolve) => setupProcess.on('exit', resolve));
+    return {};
+  } ,
+  nextConfig
+}
