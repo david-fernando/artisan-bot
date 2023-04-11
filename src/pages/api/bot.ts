6c57a205fe97944ce8ee9140d851ecc0cfc91aa7
david-fernando <davidfernandodamata21@gmail.com>
2023-04-11 19:28:50 -0300

    Update bot.ts

--- src/pages/api/bot.ts
+++ src/pages/api/bot.ts
@@ -7,13 +7,19 @@ Ainda estou em desenvolvimento, mas quando estiver pronta serei capaz de variais
 const sorryMessage = `Sinto muito, mas ainda não posso executar nenhum comando! 
 Estou em desenvolvimento, mas quando estiver pronta serei capaz de variais coisas legais como editar suas fotos, ou transforma-las em artes.`
 
+const webHookUrl = `${process.env.VERCEL_URL}/api/bot/`
 
 function apiBot(_:any, response: NextApiResponse){    
   bot.start((context) => context.reply(initialMessage));
     
   bot.on('text', (context) => context.reply(sorryMessage));
 
-  bot.launch()
+  bot.launch({
+    webhook: {
+      domain: webHookUrl,
+      secretToken: process.env.BOT_TOKEN
+    }
+  })
 
   return response.json({ message: 'All nice!' })
 
