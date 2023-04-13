3431f4af3ce15a5c26e15e6ecd0282103612b6b6
david-fernando <davidfernandodamata21@gmail.com>
2023-04-13 19:12:21 -0300

    Add grammy

--- src/pages/api/bot.ts
+++ src/pages/api/bot.ts
@@ -1,33 +1,31 @@
 import type { NextApiResponse } from 'next'
-import TelegramBot from 'node-telegram-bot-api'
+import { Bot } from "grammy";
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const webHookUrl = `${process.env.VERCEL_URL}/api/bot`
-
-
+// const webHookUrl = `${process.env.VERCEL_URL}/api/bot`
 
 const token = `${process.env.BOT_TOKEN}`
 
 const sorryMessage = `Sinto muito, mas ainda não posso executar nenhum comando! 
 Estou em desenvolvimento, mas quando estiver pronta serei capaz de variais coisas legais como editar suas fotos, ou transforma-las em artes.`
 
-const isProduction = process.env.NODE_ENV === 'production'
+// const isProduction = process.env.NODE_ENV === 'production'
 
-const bot = new TelegramBot(token, {polling: true})
+const bot = new Bot(token)
 
-if(isProduction){
-  bot.setWebHook(`${webHookUrl}/${token}`, { allowed_updates: ["message"] });
-}
+// if(isProduction){
+//   bot.setWebHook(`${webHookUrl}/${token}`, { allowed_updates: ["message"] });
+// }
+
+bot.on('message', (context) => context.reply(sorryMessage));
 
 function apiBot(_:any, response: NextApiResponse){
 
-  bot.on('message', (message) => {
-    const chatId = message.chat.id;
-  
-    bot.sendMessage(chatId, sorryMessage);
-  });
+  bot.start()
+    .then(()=> console.log('Bot iniciado'))
+    .catch((error)=> console.log(error))
 
   return response.json({ message: 'All nice!' })
 
