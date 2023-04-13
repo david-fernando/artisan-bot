ee641f5cf74520fd6d8848113a18884d1b14c535
david-fernando <davidfernandodamata21@gmail.com>
2023-04-12 23:34:09 -0300

    Add setWebHook

--- src/pages/api/bot.ts
+++ src/pages/api/bot.ts
@@ -4,21 +4,34 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const sorryMessage = `Sinto muito, mas ainda não posso executar nenhum comando! 
-Estou em desenvolvimento, mas quando estiver pronta serei capaz de variais coisas legais como editar suas fotos, ou transforma-las em artes.`
+const webHookUrl = `${process.env.VERCEL_URL}/api/bot`
+
+
 
 const token = `${process.env.BOT_TOKEN}`
 
+const sorryMessage = `Sinto muito, mas ainda não posso executar nenhum comando! 
+Estou em desenvolvimento, mas quando estiver pronta serei capaz de variais coisas legais como editar suas fotos, ou transforma-las em artes.`
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+
 const bot = new TelegramBot(token, {polling: true})
 
-bot.on('message', (message) => {
-  const chatId = message.chat.id;
-
-  bot.sendMessage(chatId, sorryMessage);
-});
+if(isProduction){
+  bot.setWebHook(`${webHookUrl}/${token}`, { allowed_updates: ["message"] });
+}
 
 function apiBot(_:any, response: NextApiResponse){
+
+  bot.on('message', (message) => {
+    const chatId = message.chat.id;
+  
+    bot.sendMessage(chatId, sorryMessage);
+  });
+
   return response.json({ message: 'All nice!' })
+
 }
 
 export default apiBot
