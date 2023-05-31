15e28032e8acd1ee8bd99a321a5853761e81fa26
david-fernando <davidfernandodamata21@gmail.com>
2023-05-31 02:02:47 -0300

    Remove handleUpdate

--- src/pages/api/bot.ts
+++ src/pages/api/bot.ts
@@ -5,9 +5,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 async function apiBot(require: NextApiRequest, response: NextApiResponse){
   await bot.init()
   try {
-    await bot.handleUpdate(require.body)
     response.statusCode = 200
-    await bot.on("message:text", (ctx) => ctx.reply("Bot em desenvolvimento"));
+    bot.on("message:text", (ctx) => ctx.reply("Bot em desenvolvimento"));
     return response.json({ message: 'Tudo certo!' })
   } catch (error) {
     console.error(error)
