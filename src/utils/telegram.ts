55161e3d97e79d5cac70cc4cf4afd9a2376004e9
david-fernando <davidfernandodamata21@gmail.com>
2023-05-31 01:10:50 -0300

    Update telegram.ts

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -1,11 +1,19 @@
 import { Telegraf } from 'telegraf';
-import * as dotenv from 'dotenv'
+const dotenv = require('dotenv')
 
 dotenv.config()
 
-const webHookUrl = `${process.env.VERCEL_URL}/api/bot/`
+// const webHookUrl = `${process.env.VERCEL_URL}/api/bot/`
 
 const bot = new Telegraf(`${process.env.BOT_TOKEN}`);
+
+bot.start((ctx) => {
+  ctx.reply('Bem-vindo ao meu bot!')
+})
+
+bot.help((ctx) => {
+  ctx.reply('Digite uma mensagem para conversar comigo.')
+})
 
 // (async()=>{
 //   await bot.launch({
