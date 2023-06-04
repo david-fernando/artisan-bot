38f723cad9178cefa0fe773af7c901fbc7dd4049
david-fernando <davidfernandodamata21@gmail.com>
2023-06-04 12:40:48 -0300

    Remove bot.init

--- src/pages/api/bot.ts
+++ src/pages/api/bot.ts
@@ -3,11 +3,9 @@ import bot from '@/utils/telegram'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 async function apiBot(require: NextApiRequest, response: NextApiResponse){
-  await bot.init()
   try {
+    await bot.handleUpdate(require.body)
     response.statusCode = 200
-    bot.command('/start', (context) => context.reply('Olá, bem vindo ao meu bot!'));
-    bot.on("message:text", (ctx) => ctx.reply("Bot em desenvolvimento"));
     return response.json({ message: 'Tudo certo!' })
   } catch (error) {
     console.error(error)
