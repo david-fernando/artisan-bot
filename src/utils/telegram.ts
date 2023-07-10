ad920605f08c87b54a24f20df6c7cce47be14e92
david-fernando <davidfernandodamata21@gmail.com>
2023-07-10 12:53:38 -0300

    Add dev command

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -1,4 +1,5 @@
 import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
 const dotenv = require('dotenv')
 
 dotenv.config()
@@ -11,16 +12,23 @@ const url = process.env.VERCEL_URL || 'artisan-bot.vercel.app'
 
 bot.telegram.setWebhook(`${url}/api/bot`)
 
-bot.start((ctx) => {
-  ctx.reply('Bem-vindo ao meu bot!')
+const reply = `Olá sou Artisan! Ainda estou em desenvolvimento, mas quando estiver pronta serei capaz de 
+transformar suas fotos em artes e varias outras coisas legais`
+
+bot.start((context) => {
+  context.reply(reply)
 })
 
-bot.help((ctx) => {
-  ctx.reply('Ainda estou em desenvolvimento.')
+bot.help((context) => {
+  context.reply('Ainda estou em desenvolvimento.')
 })
 
-bot.on('message', (ctx) => {
-  ctx.reply(`Ainda estou em desenvolvimento`)
+bot.command('dev', (context) => {
+  context.reply(reply)
+})
+
+bot.on('message', (context) => {
+  context.reply(`Ainda estou em desenvolvimento`)
 })
 
 // (async()=>{
