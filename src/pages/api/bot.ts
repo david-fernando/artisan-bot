923f9a32a9fec04b8299b851871694162f81909b
david-fernando <davidfernandodamata21@gmail.com>
2023-07-10 12:59:35 -0300

    Add await

--- src/pages/api/bot.ts
+++ src/pages/api/bot.ts
@@ -4,7 +4,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 async function apiBot(require: NextApiRequest, response: NextApiResponse){
   try {
-    bot.handleUpdate(require.body)
+    await bot.handleUpdate(require.body)
     response.statusCode = 200
     return response.json({ message: 'Tudo certo!' })
   } catch (error) {
