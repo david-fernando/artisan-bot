1de606040c37148629b39865451065dac9857a3e
david-fernando <davidfernandodamata21@gmail.com>
2023-08-05 23:22:33 -0300

    Add picWish

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -1,5 +1,6 @@
 import { Telegraf } from 'telegraf';
 import { message } from 'telegraf/filters';
+import axios from 'axios'
 const dotenv = require('dotenv')
 
 dotenv.config()
@@ -7,6 +8,8 @@ dotenv.config()
 const command:any = {}
 
 const bot = new Telegraf(`${process.env.BOT_TOKEN}`);
+
+const picWishToken = process.env.PICWISH_API_KEY
 
 const url = process.env.VERCEL_URL || 'artisan-bot.vercel.app'
 
@@ -32,16 +35,37 @@ bot.on(message('text'), (context) => {
   context.reply(reply)
 })
 
-bot.on('photo', (context) => {
-  if(command[context.from.id] === 'test'){
+bot.on('photo', async(context) => {
+  if(command[context.from.id] !== 'test'){
+    return context.reply('Digite o camando desejado!')
+  }
     const photo = context.message.photo
     const photoId = photo[photo.length - 1].file_id
-  
-    context.replyWithPhoto(photoId);
+    const fileInfo = await context.telegram.getFile(photoId)
+    const photoUrl = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${fileInfo.file_path}`;
+    const data = {
+      sync: '1',
+      image_url: photoUrl,
+      type: 'face'
+    }
+    const options = {
+      method: 'POST',
+      headers: { 
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-API-KEY': picWishToken 
+      },
+      data: JSON.stringify(data),
+      url: 'https://techhk.aoscdn.com/api/tasks/visual/scale',
+    }
+
+    const result = await axios(options)
+
+    const image = result.data.data.image
+    console.log(image)
+    context.replyWithPhoto(image);
     command[context.from.id] = ''
   }
-  return context.reply('Digite o camando desejado!')
-})
+)
 
 
 
