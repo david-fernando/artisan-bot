c8374dcf8a0c5c1864c5e7ffa7645a5907732903
david-fernando <davidfernandodamata21@gmail.com>
2023-08-11 13:57:53 -0300

    Refactor telegram.test.ts

--- src/tests/telegram.test.ts
+++ src/tests/telegram.test.ts
@@ -4,7 +4,7 @@ describe('Test user interaction with chatbot', ()=>{
   const reply = `Olá sou Artisan! Ainda estou em desenvolvimento, mas quando estiver pronta serei capaz de 
 transformar suas fotos em artes e varias outras coisas legais.`
   test("Should return a message from Chatbot", ()=>{
-    const response = botMock.start((context)=>{
+    botMock.start((context)=>{
       const response = context.reply?.(reply)
       expect(response).toBe(reply)
     })
