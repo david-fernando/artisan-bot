20242fa6d42d5dd73fb96fa2b554f64e38f0eafa
david-fernando <davidfernandodamata21@gmail.com>
2023-08-11 13:52:13 -0300

    Add telegraf.test.ts

---
+++ src/tests/telegram.test.ts
@@ -0,0 +1,12 @@
+import botMock from "../mock/telegrafMock";
+
+describe('Test user interaction with chatbot', ()=>{
+  const reply = `Olá sou Artisan! Ainda estou em desenvolvimento, mas quando estiver pronta serei capaz de 
+transformar suas fotos em artes e varias outras coisas legais.`
+  test("Should return a message from Chatbot", ()=>{
+    const response = botMock.start((context)=>{
+      const response = context.reply?.(reply)
+      expect(response).toBe(reply)
+    })
+  })
+})
