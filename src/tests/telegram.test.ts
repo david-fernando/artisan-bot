3a525fbafe079b279b69fdb764062654c53d7f02
david-fernando <davidfernandodamata21@gmail.com>
2023-08-12 00:29:12 -0300

    Add test to "melhorarnitidezfoto" command

--- src/tests/telegram.test.ts
+++ src/tests/telegram.test.ts
@@ -1,4 +1,5 @@
 import bot from "../mock/telegrafMock";
+import { enhancerSelfie } from "../utils/commands";
 
 describe('Test user interaction with chatbot', ()=>{
   const reply = `Olá sou Artisan! Ainda estou em desenvolvimento, mas quando estiver pronta serei capaz de 
@@ -14,6 +15,12 @@ transformar suas fotos em artes e varias outras coisas legais.`
     bot.help((context)=>{
       const response = context.reply?.(message)
       expect(response).toBe(message)
+    })
+  })
+  test('Should return "Já pode enviar sua foto!"', () => {
+    bot.command('melhorarnitidezfoto', (context) => { 
+      const response = enhancerSelfie(context)
+      expect(response).toBe('Já pode enviar sua foto!') 
     })
   })
 })
